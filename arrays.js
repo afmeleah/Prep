2f43dac6e9ca4e450d86436ad93904b328dc5a99
Anthony Meleah <anthony.meleah@gmail.com>
2021-11-20 10:37:54 -0500

    next exercise

--- arrays.js
+++ arrays.js
@@ -1,45 +1,47 @@
 //Search for a given number in a sorted array that has been rotated by some arbitrary number.
 
-let binarySearchRotated = function(arr, key) {
+// let binarySearchRotated = function(arr, key) {
     
-    start = 0;
-    end = arr.length - 1;
+//     start = 0;
+//     end = arr.length - 1;
 
-    if (start > end) {
-        return -1;
-    }
+//     if (start > end) {
+//         return -1;
+//     }
 
-    while (start <= end){
-        mid = start + Math.floor((end-start) /2);
+//     while (start <= end){
+//         mid = start + Math.floor((end-start) /2);
 
-        if (arr[mid] == key){
-            return mid;
-        }
+//         if (arr[mid] == key){
+//             return mid;
+//         }
 
-        if (arr[start] <= arr[mid] && key <= arr[mid] && key >= arr[start]){
-            end = mid - 1;
-        }
+//         if (arr[start] <= arr[mid] && key <= arr[mid] && key >= arr[start]){
+//             end = mid - 1;
+//         }
 
-        else if (arr[mid] <= arr[end] && key >= arr[mid] && key <= arr[end]){
-            start = mid + 1;
-        }
+//         else if (arr[mid] <= arr[end] && key >= arr[mid] && key <= arr[end]){
+//             start = mid + 1;
+//         }
 
-        else if (arr[end] <= arr[mid]){
-            start = mid + 1;
-        }
+//         else if (arr[end] <= arr[mid]){
+//             start = mid + 1;
+//         }
 
-        else if (arr[start] >= arr[mid]){
-            end = mid - 1;
-        }
+//         else if (arr[start] >= arr[mid]){
+//             end = mid - 1;
+//         }
 
-        else{
-            return -1;
-        }
-    }
-    return -1;
-};
+//         else{
+//             return -1;
+//         }
+//     }
+//     return -1;
+// };
 
-let v1 = [6, 7, 1, 2, 3, 4, 5];
+// let v1 = [6, 7, 1, 2, 3, 4, 5];
     
-console.log("Key(3) found at: " + binarySearchRotated(v1, 4));
-console.log("Key(6) found at: " + binarySearchRotated(v1, 3));
+// console.log("Key(3) found at: " + binarySearchRotated(v1, 4));
+// console.log("Key(6) found at: " + binarySearchRotated(v1, 3))
+
+//Given three integer arrays sorted in ascending order, return the smallest number that is common in all three arrays.

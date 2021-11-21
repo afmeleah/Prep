fe74dc104f50842042934c528076970658a3db40
Anthony Meleah <anthony.meleah@gmail.com>
2021-11-21 11:42:42 -0500

    low high answer

--- arrays.js
+++ arrays.js
@@ -138,3 +138,40 @@ let findLowIndex = function(arr,key) {
 
     return -1;
 }
+
+let findHighIndex = function(arr, key) {
+    let low = 0;
+    let high = arr.length - 1;
+    let mid = Math.floor(high / 2);
+
+    while (low <= high) {
+        let midElem = arr[mid];
+        if (midElem <= key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+
+        mid = low + Math.floor((high - low) / 2);
+    }
+
+    if (high === -1){
+        return high
+    }
+
+    if (high < arr.length && arr[high] === key) {
+        return high;
+    }
+
+    return -1;
+};
+
+let array = [-2, -2, -2, -2, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6];
+  
+  var key = -2;
+  console.log("Low Index of " + key + ": " + (findLowIndex(array, key)));
+  console.log("High Index of " + key + ": " + (findHighIndex(array, key)));
+  
+  key = 6;
+  console.log("Low Index of " + key + ": " + (findLowIndex(array, key)));
+  console.log("High Index of " + key + ": " + (findHighIndex(array, key)));

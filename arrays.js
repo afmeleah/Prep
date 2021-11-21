5b848e3516def0719bc5c9773a44b75e687799d8
Anthony Meleah <anthony.meleah@gmail.com>
2021-11-21 10:27:13 -0500

    more prep

--- arrays.js
+++ arrays.js
@@ -45,3 +45,36 @@
 // console.log("Key(6) found at: " + binarySearchRotated(v1, 3))
 
 //Given three integer arrays sorted in ascending order, return the smallest number that is common in all three arrays.
+
+let findLeastCommonNumber = function(a,b,c) {
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    while(i < a.length && j < b.length && k < c.length){
+
+        if (a[i] === b[j]
+            && b[j] === c[k]) {
+            return a[i];
+        }
+
+        if (a[i] <= [j] && a[i] <= c[k]) {
+            i++;
+        }
+
+        else if (b[j] <= a[i] && b[j] <= c[k]) {
+            j++;
+        }
+
+        else if (c[k] <= a[i] && c[k] <= b[j]) {
+            k++;
+        }
+    }
+
+    return -1;
+};
+
+let v1 = [4, 6, 7, 10, 25, 30, 63, 64];
+let v2 = [0, 4, 5, 6, 7, 8, 50];
+let v3 = [1, 4, 6, 10, 14];
+console.log("Least Common Number: " + findLeastCommonNumber(v1, v2, v3));
